d07b0e1f6033ca246b8558d2c15758b3c6fdf69f
hrach105 <hrachkhachatryan24@gmail.com>
2022-07-17 14:03:00 +0400

    header footer home page banner

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.selectpicker').selectpicker();
+});
+
+
+$('.banner-slider').slick({
+    dots:true,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='./assets/img/arrow-left.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='./assets/img/arrow-right.png'>"
+});
