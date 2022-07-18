08602a6fd68125ab5acd2bc2ff8c81aaf99eb504
hrach105 <63844723+hrach105@users.noreply.github.com>
2022-07-18 14:29:57 +0400

    add brand page  and global  component

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,13 @@ $('.banner-slider').slick({
     prevArrow:"<img class='a-left control-c prev slick-prev' src='./assets/img/arrow-left.png'>",
     nextArrow:"<img class='a-right control-c next slick-next' src='./assets/img/arrow-right.png'>"
 });
+
+
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:"<div class='slick-left-arrow'><img class='a-left control-c arrow prev slick-prev' src='../../assets/img/arrow.png'></div>",
+    nextArrow:"<div class='slick-right-arrow'><img class='a-right control-c arrow next slick-next' src='../../assets/img/arrow.png''></div>"
+});
